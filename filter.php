0a85060f28a7700c4c5e1172f02dd4ee182e9a08
Mehedee <mehedee@opcodespace.com>
2018-05-02 19:03:47 +0600

    Update filter.php

--- filter.php
+++ filter.php
@@ -1,9 +1,6 @@
 <?php 
 function replace_customer_area_title( $wp_admin_bar ) {
-
     //$subdomain = explode('.', $_SERVER['HTTP_HOST'])[0];
-
    $wp_admin_bar->add_node( array( 'id' => 'site-name', 'title' => __('test', 'domain) ) );
 }
-
 add_filter( 'admin_bar_menu', 'replace_customer_area_title' , 33 );
