6fbba9715d44510c62b836f3e0637d042872a2c9
Mehedee <mehedee@opcodespace.com>
2018-05-08 00:10:17 +0600

    Update hooks.php

--- hooks.php
+++ hooks.php
@@ -12,3 +12,28 @@ function redirect_from_mission()
 	}
 }
 add_action( 'template_redirect', 'redirect_from_mission', 10, 1 );
+
+
+
+/**
+ * Page Visibility in wp admin as per user role
+ * @return type
+ */
+function exclude_this_page($query)
+{
+    global $current_user;
+    $user_roles = $current_user->roles;
+
+    //Ids of pages to be excluded
+    $excluded_pages = [];
+
+    global $pagenow;
+
+    if ('edit.php' == $pagenow && (get_query_var('post_type') && 'page' == get_query_var('post_type'))) {
+        if (in_array('webmaster', $user_roles)) {
+            $query->set('post__not_in', $excluded_pages);
+        }
+    }
+    return $query;
+}
+add_action('pre_get_posts', 'exclude_this_page');
