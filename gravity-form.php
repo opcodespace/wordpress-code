6ef452ca1f36a1d1dca2a53e6704e86c25104263
Mehedee <mehedee@opcodespace.com>
2018-08-02 20:37:09 +0600

    Create gravity-form.php

---
+++ gravity-form.php
@@ -0,0 +1,10 @@
+/**
+* Gravity Wiz // Gravity Forms // Skip Pages on Multi-Page Form
+* https://gravitywiz.com/2012/05/04/pro-tip-skip-pages-on-multi-page-forms/
+*/
+add_filter("gform_pre_render", "gform_skip_page");
+function gform_skip_page($form) {
+ if(!rgpost("is_submit_{$form['id']}") && rgget('form_page') && is_user_logged_in())
+ GFFormDisplay::$submission[$form['id']]["page_number"] = rgget('form_page');
+ return $form;
+}
