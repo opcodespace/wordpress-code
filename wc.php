0b61c6968e3302eab97dac6a8ed595cdf9f71861
Mehedee <mehedee@opcodespace.com>
2018-08-02 20:39:34 +0600

    Update wc.php

--- wc.php
+++ wc.php
@@ -1,4 +1,35 @@
 <?php
+
+WC_Order::get_checkout_payment_url( $on_checkout )
+
+
+# order associated with customer
+$customer_orders = get_posts( array(
+    'numberposts' => -1,
+    'meta_key'    => '_customer_user',
+    'meta_value'  => get_current_user_id(),
+    'post_type'   => wc_get_order_types(),
+    'post_status' => array_keys( wc_get_order_statuses() ),
+) )
+
+# Status 
+Array
+(
+    [wc-pending] => Pending payment
+    [wc-processing] => Processing
+    [wc-on-hold] => On hold
+    [wc-completed] => Completed
+    [wc-cancelled] => Cancelled
+    [wc-refunded] => Refunded
+    [wc-failed] => Failed
+)
+
+# update wc status 
+$order = wc_get_order( $order_details->ID );
+$result = $order->set_status("pending", "cancelled");
+$order->save();
+
+
 /**
  * Optimize WooCommerce Scripts
  * Remove WooCommerce Generator tag, styles, and scripts from non WooCommerce pages.
